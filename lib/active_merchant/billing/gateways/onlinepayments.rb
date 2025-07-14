c5112ec15f24749a5678de1e6521390d15c48a18
Jonathon Davis <jed@rgnets.com>
2025-07-14 16:21:16 -0500

    Set default URL to anz preprod

--- lib/active_merchant/billing/gateways/onlinepayments.rb
+++ lib/active_merchant/billing/gateways/onlinepayments.rb
@@ -27,8 +27,8 @@ module ActiveMerchant #:nodoc:
         @api_key_id = options[:login]
         @secret_api_key = options[:password]
         @integrator = 'ActiveMerchant'
-        @client = build_client
-        @api_endpoint = options[:live_url]
+        # Use options[:live_url] if provided, otherwise use the default production endpoint
+        @api_endpoint = options[:live_url] || 'https://payment.preprod.anzworldline-solutions.com.au/'
         super
       end
 
